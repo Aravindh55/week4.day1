94cf694955f20769f5f8fc3371a5013aab934e99
Aravindh55 <91793881+Aravindh55@users.noreply.github.com>
2021-10-24 17:02:00 +0530

    Add files via upload

---
+++ FramesAssign.java
@@ -0,0 +1,36 @@
+package week4.day1;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FramesAssign {
+
+	public static void main(String[] args) throws IOException {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leafground.com/pages/frame.html");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebElement element = driver.findElement(By.xpath("(//div[@id='wrapframe']/iframe)[1]"));
+		driver.switchTo().frame(element);
+		WebElement element2 = driver.findElement(By.id("Click"));
+		File a = element2.getScreenshotAs(OutputType.FILE);
+		File b = new File("./snaps/button.png");
+		FileUtils.copyFile(a, b);
+		driver.switchTo().defaultContent();
+		List<WebElement> findElements = driver.findElements(By.tagName("iframe"));
+		System.out.println("the number of frames is: "+ findElements.size());
+
+	}
+
+}
